b2035c6c5d681b9854d6c58ffda9ac5e360900f9
Will Schlitzer <schlitzer90@gmail.com>
2021-12-31 10:20:00 +0000

    add login flash messages

--- app/app.py
+++ app/app.py
@@ -45,9 +45,15 @@ def register():
         return redirect(url_for("home"))
     return render_template("register.html", title="Register", form=form)
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check e-mail and password", "danger")
     return render_template("login.html", title="Login", form=form)
 
 
