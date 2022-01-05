ac2a8c81c1c3d335fd4ef1a44e69f57c1fc2f0e2
Will Schlitzer <schlitzer90@gmail.com>
2022-01-05 11:33:32 +0000

    update user authentication and login

--- silly_sports_stats/routes.py
+++ silly_sports_stats/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from silly_sports_stats import app, db, bcrypt
 from silly_sports_stats.forms import RegistrationForm, LoginForm
 from silly_sports_stats.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 
@@ -39,6 +39,8 @@ def about():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
@@ -52,12 +54,25 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for("home"))
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
             flash("Login Unsuccessful. Please check e-mail and password", "danger")
     return render_template("login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
