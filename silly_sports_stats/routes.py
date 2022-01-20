cfaf22cd73b69c8876a014c8c4b97e0ea0d571d4
Will Schlitzer <schlitzer90@gmail.com>
2022-01-20 09:09:55 +0000

    update for profile picture changing

--- silly_sports_stats/routes.py
+++ silly_sports_stats/routes.py
@@ -1,9 +1,10 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 from silly_sports_stats import app, db, bcrypt
-from silly_sports_stats.forms import RegistrationForm, LoginForm
+from silly_sports_stats.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from silly_sports_stats.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
 
 
 posts = [
@@ -43,8 +44,12 @@ def register():
         return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            username=form.username.data, email=form.email.data, password=hashed_password
+        )
         db.session.add(user)
         db.session.commit()
         flash(f"Your account has been created! You are now able to log in!", "success")
@@ -67,12 +72,38 @@ def login():
             flash("Login Unsuccessful. Please check e-mail and password", "danger")
     return render_template("login.html", title="Login", form=form)
 
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+    form_picture.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("You account has been updated", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
